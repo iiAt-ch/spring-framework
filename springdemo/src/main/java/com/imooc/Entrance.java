8f01ad00fc09cf9f2d22860984e00120c23ec6e9
chenhui <1499282788@qq.com>
2021-06-26 13:04:30 +0800

    springdemo

---
+++ springdemo/src/main/java/com/imooc/Entrance.java
@@ -0,0 +1,37 @@
+package com.imooc;
+
+import com.imooc.aspect.OutSide;
+import com.imooc.controller.HelloController;
+import com.imooc.controller.HiController;
+import com.imooc.controller.WelcomeController;
+import com.imooc.dao.impl.Company;
+import com.imooc.entity.User;
+import com.imooc.entity.factory.UserFactoryBean;
+import com.imooc.introduction.LittleUniverse;
+import com.imooc.service.HelloService;
+import com.imooc.service.HiService;
+import com.imooc.service.WelcomeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+//@Configuration
+//@EnableAspectJAutoProxy
+//@Import(OutSide.class)
+//@ComponentScan("com.imooc")
+public class Entrance {
+
+	public static void main(String[] args) {
+		String xmlPath = "D:\\Project\\spring-framework\\springdemo\\src\\main\\java\\com\\imooc\\Entrance.java";
+		ApplicationContext applicationContext = new FileSystemXmlApplicationContext(xmlPath);
+		WelcomeService welcomeService = (WelcomeService) applicationContext.getBean("welcomeService");
+		welcomeService.sayHello("强大的spring框架");
+
+//		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Entrance.class);
+//		HiService hiService = (HiService)applicationContext.getBean("hiServiceImpl");
+//		hiService.sayHi();
+//		System.out.println("---------------------------分割线以下执行HelloService-------------------------------");
+//		HelloService helloService = (HelloService)applicationContext.getBean("helloServiceImpl");
+//		helloService.sayHello();
+	}
+}
