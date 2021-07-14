10d9677096388baa6bab4864fbe4dc07de510502
iiAt <149928788@qq.com>
2021-07-14 23:36:22 +0800

    格式化

--- springdemo/src/main/java/com/imooc/controller/WelcomeController.java
+++ springdemo/src/main/java/com/imooc/controller/WelcomeController.java
@@ -12,26 +12,27 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class WelcomeController implements ApplicationContextAware, BeanNameAware {
-	private String myName;
-	private ApplicationContext myContainer;
-	@Autowired
-	private WelcomeService welcomeService;
-	public void handleRequest(){
-		welcomeService.sayHello("来自Controller的问候");
-		System.out.println("我是谁：" + myName);
-		String[] beanDefinitionNames = myContainer.getBeanDefinitionNames();
-		for(String beanDefinitionName : beanDefinitionNames) {
-			System.out.println("召唤容器神兽，通过神兽获得：" + beanDefinitionName);
-		}
-	}
+    private String myName;
+    private ApplicationContext myContainer;
+    @Autowired
+    private WelcomeService opewelcomeService;
 
-	@Override
-	public void setBeanName(String name) {
-		this.myName = name;
-	}
+    public void handleRequest() {
+        welcomeService.sayHello("来自Controller的问候");
+        System.out.println("我是谁：" + myName);
+        String[] beanDefinitionNames = myContainer.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("召唤容器神兽，通过神兽获得：" + beanDefinitionName);
+        }
+    }
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.myContainer = applicationContext;
-	}
+    @Override
+    public void setBeanName(String name) {
+        this.myName = name;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.myContainer = applicationContext;
+    }
 }
